47f2f238b51f093210b655cd32222388fde86a88
rimelJB <rimel1jabnoun@gmail.com>
2021-10-20 22:41:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import UserList from './UserList';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/UserList.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const UserList = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setList(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div className="profile">
+      {list.map((user) => {
+          return(
+    <div>
+     <div className="card" style={{ width: "18em",
+     margin: "20px",
+     height: "11rem"}}>
+     <div className="card-header">
+        <h6> {user.name}</h6>
+     </div>
+     <ul className="list-group list-group-flush">
+        <li className="list-group-item" key={user.id}>{user.email}</li>
+        <li className="list-group-item">{user.phone}</li>
+        <li className="list-group-item">{user.website}</li>
+    </ul>
+    </div>
+    </div>
+        );
+      })}
+         
+    </div>
+  );
+};
+
+
+  
+export default UserList;
